22da070c51831595ae71d790b43b0846b73a711f
aklimanov <mail@yourmail.ru>
2022-03-05 01:13:07 +0300

    catalog. preproc, anime

--- script.js
+++ script.js
@@ -2,9 +2,27 @@ let panelMenu = document.querySelector('.menu-panel');
 let menuButton = document.querySelector('.menu');
 let menuClose = document.querySelector('.close-img');
 
-function toggleMenuPanel() {
-    panelMenu.classList.toggle('hidden');
-}
+let sizeFilterBody = document.querySelector('.fltrsize-body');
+let sizeFilter = document.querySelector('.fltrsize-name');
+let sizeFilterDown = document.querySelector('.fltrsize img');
 
-menuButton.addEventListener('click', toggleMenuPanel);
-menuClose.addEventListener('click', toggleMenuPanel);
+let anime = document.querySelector('.test-anime');
+let filter = document.querySelector('.filter-logo');
+let filteActive = document.querySelector('.filterActive-fat');
+
+menuButton.addEventListener('click', function(){ panelMenu.classList.toggle('hidden') });
+menuClose.addEventListener('click', function(){ panelMenu.classList.toggle('hidden') });
+sizeFilter.addEventListener('click', function(){ sizeFilterBody.classList.toggle('hidden') });
+sizeFilterDown.addEventListener('click', function(){ sizeFilterBody.classList.toggle('hidden') });
+filter.addEventListener('click', function(){ anime.classList.toggle('onAnime') });
+filteActive.addEventListener('click', function(){ anime.classList.toggle('onAnime') });
+
+let filterCategories = document.querySelectorAll('.category-head');
+filterCategories.forEach(function (category) {
+    category.addEventListener('click', ({target}) => {
+        filterCategories.forEach(function (cat) {
+            cat.classList.remove('active')
+        });
+        target.classList.add('active');
+    })
+});
