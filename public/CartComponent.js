7a74aaa1cda6e61c3d14edb9245966ee3ec8c693
Al Ex <web-html-css@yandex.ru>
2022-06-06 06:56:38 +0300

    products from server

---
+++ public/CartComponent.js
@@ -0,0 +1,98 @@
+Vue.component('cart', {
+    data(){
+      return {
+          cartUrl: '/getBasket.json',
+          cartItems: [],
+          showCart: false,
+          amount: 0,
+          countGoods: 0
+      }
+    },
+    mounted(){
+        this.$parent.getJson(`/api/cart`)
+            .then(data => {
+                for (let item of data.contents){
+                    this.$data.cartItems.push(item);
+                }
+                this.$data.amount = data.amount;
+                this.$data.countGoods = data.countGoods;
+            });
+            
+    },
+    methods: {
+        addProduct(item){
+            let find = this.cartItems.find(el => el.id_product === item.id_product);
+            if(find){
+                this.$parent.putJson(`/api/cart/${find.id_product}`, {quantity: 1})
+                    .then(data => {
+                        if(data.result === 1){
+                            find.quantity++
+                        }
+                    })
+            } else {
+                const prod = Object.assign({quantity: 1}, item);
+                this.$parent.postJson(`/api/cart`, prod)
+                    .then(data => {
+                        if(data.result === 1){
+                            this.cartItems.push(prod)
+                        }
+                    })
+                this.countGoods++;
+            }
+            this.amount += item.price;
+        
+        },
+        removeProduct(item){
+            let find = this.cartItems.find(el => el.id_product === item.id_product);
+            this.$parent.deleteJson(`/api/cart/${ item.id_product }`)
+            .then(data => {
+                if (data.result) {
+                    this.cartItems.splice(this.cartItems.indexOf(item), 1);
+                    this.countGoods--;
+                    this.amount -= (find.price * find.quantity);
+                } else {
+                    console.log('error');
+                }
+            })
+        },
+    },
+    template: `<div style="display: inline-block">
+               <a href="cart/cart.html" @mouseover="showCart = true" @mouseout="showCart = false" class="menu-p cart">
+                   <div class="cart-items">
+                       <img src="img/headcart.png" alt="" class="menu-img">
+                       <span>{{ countGoods }}</span>
+                   </div>
+               </a>
+
+               <div class="cart-panel" v-show="showCart">
+                   <div class="cartPanel-header">
+                       <div class="prod-name">Товар</div>
+                       <div>Кол-во</div>
+                       <div>Цена</div>
+                       <div>Сумма</div>
+                   </div>
+                   <hr>
+                   <div class="cartPanel-items">
+                       <cart-item v-for="item of cartItems" :key="item.id_product" :cart-item="item">
+                       </cart-item>
+                   </div>
+                   <h3 v-show="cartItems.length==0">Нет данных...</h3>
+                   <div class="cartPanel-total">
+                       Итого: <span class="cart-total">{{ amount.toFixed(2) }}</span> $
+                   </div>
+               </div>
+               </div>
+    `
+});
+
+Vue.component('cart-item', {
+    props: ['cartItem'],
+    template: `<div class="cartPanel-item"">
+                   <div class="prod-name">{{ cartItem.product_name }}</div>
+                   <div>{{ cartItem.quantity }}</div>
+                   <div>{{ cartItem.price }}</div>
+                   <div>{{(cartItem.quantity*cartItem.price).toFixed(2)}}</div>
+                   <button class="del-btn" @click="$parent.removeProduct(cartItem)">&times;</button>
+               </div>
+    `
+})
---
+++ public/ProductComponent.js
@@ -0,0 +1,59 @@
+Vue.component('products', {
+   data(){
+       return {
+           catalogUrl: '/catalogData.json',
+           filtered: [],
+           products: [],
+       }
+   },
+    mounted(){
+        this.$parent.getJson(`/api/products`)
+            .then(data => {
+                for (let item of data){
+                    this.$data.products.push(item);
+                    this.$data.filtered.push(item);
+                }
+            });
+    },
+    methods: {
+        filter(userSearch){
+            let regexp = new RegExp(userSearch, 'i');
+            this.filtered = this.products.filter(el => regexp.test(el.product_name));
+        }
+    },
+   template: `<div class="fetured-container">
+                <product v-for="item of filtered" 
+                :key="item.id_product" 
+                :product="item">
+                </product>
+               </div>`
+});
+Vue.component('product', {
+    props: ['product'],
+    template: `
+            <div class="fetured-block">
+                <div class="fetured-img">
+                    <img :src="product.img" alt="Some img">
+                    <div class="hover-fog">
+                        <button class="hover-fog-btn" @click="$root.$refs.cart.addProduct(product)">
+                            <img src="img/Vector.svg" alt="cart">
+                            Add to Cart
+                        </button>
+                    </div>
+                </div>
+                <div class="fetured-txt">
+                    <a href="product/product.html">
+                        <div class="fetured-txt-title">
+                            {{product.product_name}}
+                        </div>
+                        <div class="fetured-txt-body">
+                            {{product.desc}}
+                        </div>
+                        <div class="fetured-txt-prise red-txt">
+                            $ {{product.price}}
+                        </div>
+                    </a>
+                </div>
+            </div>
+    `
+})
